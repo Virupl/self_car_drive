d94a65e2b85bbc0d899e1e6c0d35e6cba8ba62d9
panchalviresh757 <panchalviresh757@gamil.com>
2023-07-19 18:33:49 +0530

    changes remove in conf file

--- 2_Self/conf.py
+++ 2_Self/conf.py
@@ -24,8 +24,6 @@ road = Road(screen_width / 2, screen_width * 0.9)
 
 # Create a surface for the car image
 car_image = pygame.Surface((30, 50), pygame.SRCALPHA)
-sdaasdasd = "asdasdasd"
-abc = "abc"
-xyz = "asdsad"
+
 # Detected Cars
 detected_cars = []
