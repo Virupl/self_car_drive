6fc243cfda8a4d525a058e73da9308d44600a146
panchalviresh757 <panchalviresh757@gamil.com>
2023-07-19 18:16:09 +0530

    conflict learn

--- 2_Self/conf.py
+++ 2_Self/conf.py
@@ -24,7 +24,7 @@ road = Road(screen_width / 2, screen_width * 0.9)
 
 # Create a surface for the car image
 car_image = pygame.Surface((30, 50), pygame.SRCALPHA)
-sdaasdasd="asdasdasd"
-sdaasdasd="asdasdasd"
+sdaasdasd = "asdasdasd"
+abc = "abc"
 # Detected Cars
 detected_cars = []
