27a6073f0cc89fb48e70a0ce844d5ac355dd11c5
panchalviresh757 <panchalviresh757@gamil.com>
2023-07-20 16:43:43 +0530

    car generate

--- 2_Self/Network.py
+++ 2_Self/Network.py
@@ -1,9 +1,20 @@
 import random
+import math
 from utils import lerp
 
 
 class NeuralNetwork:
-    def __init__(self, neuronCounts):
+    def __init__(self, neuronCounts, input_size=6, hidden_size=6, output_size=4):
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.weights_input_hidden = self.initialize_weights(
+            input_size, hidden_size)
+        self.weights_hidden_output = self.initialize_weights(
+            hidden_size, output_size)
+        self.biases_hidden = self.initialize_biases(hidden_size)
+        self.biases_output = self.initialize_biases(output_size)
+
         self.levels = []
         for i in range(0, len(neuronCounts) - 1):
             self.levels.append(Level(neuronCounts[i], neuronCounts[i+1]))
@@ -16,16 +27,56 @@ class NeuralNetwork:
 
         return outputs
 
+    # @staticmethod
+    # def mutate(network, amount=1):
+    #     for level in network.levels:
+    #         for i in range(len(level.biases)):
+    #             level.biases[i] = lerp(
+    #                 level.biases[i], random.uniform(-1, 1), amount)
+    #         for i in range(len(level.weights)):
+    #             for j in range(len(level.weights[i])):
+    #                 level.weights[i][j] = lerp(
+    #                     level.weights[i][j], random.uniform(-1, 1), amount)
+
+    def initialize_weights(self, input_size, output_size):
+        return [[random.uniform(-1, 1) for _ in range(output_size)] for _ in range(input_size)]
+
+    def initialize_biases(self, size):
+        return [random.uniform(-1, 1) for _ in range(size)]
+
+    def sigmoid(self, x):
+        return 1 / (1 + math.exp(-x))
+
+    def forward(self, input_data):
+        hidden_layer = [self.sigmoid(sum([input_data[j] * self.weights_input_hidden[i][j] for j in range(
+            self.input_size)]) + self.biases_hidden[i]) for i in range(self.hidden_size)]
+        output_layer = [self.sigmoid(sum([hidden_layer[j] * self.weights_hidden_output[i][j]
+                                     for j in range(self.hidden_size)]) + self.biases_output[i]) for i in range(self.output_size)]
+        return output_layer
+
     @staticmethod
-    def mutate(network, amount=1):
-        for level in network.levels:
-            for i in range(len(level.biases)):
-                level.biases[i] = lerp(
-                    level.biases[i], random.uniform(-1, 1), amount)
-            for i in range(len(level.weights)):
-                for j in range(len(level.weights[i])):
-                    level.weights[i][j] = lerp(
-                        level.weights[i][j], random.uniform(-1, 1), amount)
+    def mutate(self, mutation_rate, mutation_amount=0.1):
+        for i in range(self.input_size):
+            for j in range(self.hidden_size):
+                if random.random() < mutation_rate:
+                    self.weights_input_hidden[i][j] += random.uniform(
+                        -mutation_amount, mutation_amount)
+
+        for i in range(self.hidden_size):
+            for j in range(self.output_size):
+                if random.random() < mutation_rate:
+                    self.weights_hidden_output[i][j] += random.uniform(
+                        -mutation_amount, mutation_amount)
+
+        for i in range(self.hidden_size):
+            if random.random() < mutation_rate:
+                self.biases_hidden[i] += random.uniform(-mutation_amount,
+                                                        mutation_amount)
+
+        for i in range(self.output_size):
+            if random.random() < mutation_rate:
+                self.biases_output[i] += random.uniform(-mutation_amount,
+                                                        mutation_amount)
 
 
 class Level:
